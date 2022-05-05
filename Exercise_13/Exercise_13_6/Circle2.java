8ffd39f471beebc1fef94a35dfe24502ca4edf53
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-05-05 18:57:36 +0600

    Create Circle2.java

---
+++ Exercise_13/Exercise_13_6/Circle2.java
@@ -0,0 +1,56 @@
+public class Circle2 extends GeometricObject{
+    private double x;
+    private double y;
+    private double radius;
+
+    public Circle2(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+    @Override
+    public int compareTo(GeometricObject o) {
+        if (getArea() > o.getArea())
+            return 1;
+        else if (getArea() < o.getArea())
+            return -1;
+        else
+            return 0;
+    }
+}
