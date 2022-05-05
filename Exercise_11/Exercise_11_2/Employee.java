79ed108cd92476be15eaeca4bed46fbc1cfc3fd2
Mutasim <96326525+mutasim77@users.noreply.github.com>
2022-05-05 18:48:31 +0600

    Create Employee.java

---
+++ Exercise_11/Exercise_11_2/Employee.java
@@ -0,0 +1,32 @@
+public class Employee extends Person{
+    private int office;
+    private double salary;
+
+    public Employee(String name, String address, String phone, String email, int office, double salary) {
+        super(name, address, phone, email);
+        this.office = office;
+        this.salary = salary;
+    }
+
+    public int getOffice() {
+        return office;
+    }
+
+    public void setOffice(int office) {
+        this.office = office;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nOffice: " + office + "\nSalary: $" + getSalary();
+    }
+
+}
